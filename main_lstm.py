b7601da355eaa8aa7d52500da70ee40b7c7775e6
Alessandro Censi <alessandromic.censi@gmail.com>
2020-03-05 15:04:29 +0100

    Model Saving and Loading added.

--- main_lstm.py
+++ main_lstm.py
@@ -1,12 +1,14 @@
 import numpy as np
-import constants
+import tensorflow as tf
+import os
+from constants import N_CLASSES, EPOCHS, N_TIMESTAMPS, N_FOLDS, BATCH_SIZE, EXTENSION, BASE_DIR_FOLDS, BASE_FILE_NAMES
 from model import MyLSTM
 from utils import reshapeToTensor
 from sklearn.metrics import accuracy_score, f1_score, cohen_kappa_score
 from train import train
 
 
-for i in range(constants.N_FOLDS):
+for i in range(N_FOLDS):
     current_fold = str(i + 1)
     train_fn = '[YOUR TRAIN DATA FILE PATH HERE]'
     validation_fn = '[YOUR VALIDATION DATA FILE PATH HERE]'
@@ -14,6 +16,7 @@ for i in range(constants.N_FOLDS):
     target_train_fn = '[YOUR TRAIN LABELS FILE PATH HERE]'
     target_validation_fn = '[YOUR VALIDATION LABELS FILE PATH HERE]'
     target_test_fn = '[YOUR TEST LABELS FILE PATH HERE]'
+    
     # loading the data already splitted
     x_train = np.load(train_fn)
     x_validation = np.load(validation_fn)
@@ -28,12 +31,22 @@ for i in range(constants.N_FOLDS):
     y_train = np.load(target_train_fn)
     y_validation = np.load(target_validation_fn)
     y_test = np.load(target_test_fn)
-    n_classes = 12
-    model = MyLSTM(512, n_classes, dropout_rate=0.5)
+    model = MyLSTM(512, N_CLASSES, dropout_rate=0.5)
     print(x_train.shape)
     print("Fold %s metrics:\n" % current_fold)
+
     # TRAINING
-    train(model, x_train, y_train, x_validation, y_validation, n_epochs=constants.EPOCHS)
+    outputFolder = './MLP_output'
+    if not os.path.exists(outputFolder):
+        os.makedirs(outputFolder)
+
+    """ defining loss function and the optimizer to use in the training phase """
+    loss_object = tf.keras.losses.CategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+
+    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, model=model)
+    manager = tf.train.CheckpointManager(ckpt, outputFolder + '/MLP_ckpts_fold ' + current_fold, max_to_keep=1)
+    train(model, x_train, y_train, x_validation, y_validation, loss_object, optimizer, ckpt, manager, n_epochs=EPOCHS)
 
     # TESTING
     pred = model.predict(x_test)
--- train.py
+++ train.py
@@ -2,19 +2,23 @@ import tensorflow as tf
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
-from utils import getBatch
+from  utils import getBatch
 
 
-def train(model, x_train, y_train, x_validation, y_validation, learning_rate=0.0005, batch_size = 32, n_epochs = 10):
-
-    """ defining loss function and the optimizer to use in the training phase """
-    loss_object = tf.keras.losses.CategoricalCrossentropy()
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+def train(model, x_train, y_train, x_validation, y_validation, loss_object, optimizer, ckpt, manager, batch_size = 32, n_epochs = 10):
+    # trying to restore a previous checkpoint. If it fails, starts from scratch
+    ckpt.restore(manager.latest_checkpoint)
+    if manager.latest_checkpoint:
+        print("Restored from {}".format(manager.latest_checkpoint))
+    else:
+        print("Initializing from scratch.")
 
     iterations = x_train.shape[0] / batch_size
     if x_train.shape[0] % batch_size != 0:
         iterations += 1
 
+    best_step = -1
+    best_epoch_val_loss = 100.0
     for e in range(n_epochs):
         loss_iteration = 0
         x_train, y_train = shuffle(x_train, y_train, random_state=0)
@@ -28,7 +32,6 @@ def train(model, x_train, y_train, x_validation, y_validation, learning_rate=0.0
                 loss = loss_object(batch_y, predictions)
                 loss_iteration += loss.numpy()
                 gradients = tape.gradient(loss, model.trainable_variables)
-                #print("gradients ",gradients)
                 optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
         total_loss = loss_iteration / int(iterations)
@@ -37,6 +40,17 @@ def train(model, x_train, y_train, x_validation, y_validation, learning_rate=0.0
         y_argmax = np.argmax(y_train, axis=1)
         pred_argmax = np.argmax(pred, axis=1)
         ac = accuracy_score(y_argmax, pred_argmax)
+
+        # increment of checkpoint step
+        ckpt.step.assign_add(1)
+        if total_loss <= best_epoch_val_loss:
+            # new best model found, so save the checkpoint into a file
+            best_epoch_val_loss = total_loss
+            best_step = int(ckpt.step)
+            save_path = manager.save()
+            print("Saved checkpoint for step {}: {}".format(best_step, save_path))
+            print("loss {:1.2f}".format(best_epoch_val_loss))
+
         print("epoch %d loss %f Train Accuracy %f" % (
             e, total_loss, np.round(ac, 4)))
         pred = model(x_validation, training=False)
